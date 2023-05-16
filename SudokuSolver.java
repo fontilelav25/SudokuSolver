5daffb871ed6808461653b0935b2d86e913d26f1
fontilelav25 <133689260+fontilelav25@users.noreply.github.com>
2023-05-16 21:19:55 +0300

    Create SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,64 @@
+public class SudokuSolver {
+    public static void sudoku(int[][] puzzle) {
+        solveSudoku(puzzle);
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        int row = -1;
+        int col = -1;
+        boolean isEmpty = true;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty) {
+                break;
+            }
+        }
+
+        if (isEmpty) {
+            return true;
+        }
+
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+
+                if (solveSudoku(board)) {
+                    return true;
+                }
+
+                board[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
